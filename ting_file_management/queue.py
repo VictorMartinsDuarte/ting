2e927448c229201b23ae7382d4f8d899d0ca9dcf
Victor Martins <victor.martins.duarte@gmail.com>
2022-10-27 16:25:10 -0300

    Feat: Req-01 Implemented class 'Queue' methods

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -9,7 +9,9 @@ class Queue:
         self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index <= len(self._data):
+            return self._data[index]
+        raise IndexError
