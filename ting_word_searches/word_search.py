46654f8c6b910baa96abf5b6060b7df741409bbf
Victor Martins <victor.martins.duarte@gmail.com>
2022-10-27 19:08:56 -0300

    Chore: Req-06 Adds logic to 'exists_word' function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
+def all_processed_files(word, instance):
+    return [
+        {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": file["linhas_do_arquivo"],
+        }
+        for file in instance._data
+    ]
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files_data = all_processed_files(word, instance)
+    for file in files_data:
+        file["ocorrencias"] = [
+            {"linha": i + 1}
+            for i, line in enumerate(file["ocorrencias"])
+            if word.lower() in line.lower()
+        ]
+    if not file["ocorrencias"]:
+        return []
+        
+    file_info = []
+    file_info.append(file)
+    return file_info
 
 
 def search_by_word(word, instance):
