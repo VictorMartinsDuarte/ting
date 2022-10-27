00b3f67c71c3301b8abaeaa7ffbb3fe76ca5cb4f
Victor Martins <victor.martins.duarte@gmail.com>
2022-10-27 16:46:45 -0300

    Chore: Req-03 Adds logic to 'process' function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_lines = txt_importer(path_file)
+    processed_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines,
+    }
+
+    if processed_file in instance._data:
+        return None
+
+    instance.enqueue(processed_file)
+    print(processed_file)
 
 
 def remove(instance):
